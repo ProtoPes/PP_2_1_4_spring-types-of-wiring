47fbf6994788f4fc726c1f362f02bfe2f1ac73ea
ProtoPes <ebu.krota@gmail.com>
2023-03-29 21:14:51 +0300

    We have full phrase

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,7 +1,6 @@
 package koschei.config;
 
-import koschei.models.Island2;
-import koschei.models.Wood3;
+import koschei.models.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +8,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "koschei")
 public class AppConfig {
+
+    @Bean
+    public static Deth8 getDeth() {
+        return new Deth8();
+    }
+
+    @Bean
+    public static Needle7 getNeedle(Deth8 deth) {
+        return new Needle7(deth);
+    }
+
+    @Bean
+    public static Egg6 getEgg(Needle7 needle) {
+        return new Egg6(needle);
+    }
 
     @Bean
     public static Island2 getIsland(Wood3 wood) {
